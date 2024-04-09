aac03e3526d2406e826540b8043a1b6c741f55fa
Hariharan1595 <132120282+Hariharan1595@users.noreply.github.com>
2024-04-09 20:15:02 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+      <p>
+        Coded by
+        <a href="#"> Hariharan</a>,
+        <a href="https://github.com/Hariharan1595">Open sourced on Github.</a>
+      </p>
+    </div>
+  );
+}
